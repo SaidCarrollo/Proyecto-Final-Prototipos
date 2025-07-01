6e68b3d7369567e778af292411c3e79633241251
SaidCarrollo <harvy.carrollo@tecsup.edu.pe>
2025-07-01 02:16:59 -0400

    FewChanges

--- Assets/Scrips/Audio/AudioSettingsController.cs
+++ Assets/Scrips/Audio/AudioSettingsController.cs
@@ -1,67 +1,34 @@
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
-using UnityEngine.InputSystem;
+
 public class AudioSettingsController : MonoBehaviour
 {
     [Header("UI Elements")]
-    public GameObject audioSettingsPanel;
     public Slider musicSlider;
     public Slider sfxSlider;
 
     [Header("Audio")]
     public AudioMixer audioMixer;
 
-    private bool isPaused = false;
-    [SerializeField] private FirstPersonController playerController; 
-
-    [Header("Input Actions")]
-    [Tooltip("Acción de input para pausar el juego.")]
-    [SerializeField] private InputActionReference pauseAction;
-
-
     void Awake()
     {
+        // Inicializa los sliders con los valores actuales del mixer
         float musicVolume;
         if (audioMixer.GetFloat("MusicVolume", out musicVolume))
         {
             musicSlider.value = musicVolume;
         }
+
         float sfxVolume;
         if (audioMixer.GetFloat("SFXVolume", out sfxVolume))
         {
             sfxSlider.value = sfxVolume;
         }
+
+        // Añade los listeners para cuando cambien los valores
         musicSlider.onValueChanged.AddListener(SetMusicVolume);
         sfxSlider.onValueChanged.AddListener(SetSFXVolume);
-
-
-        audioSettingsPanel.SetActive(false);
-    }
-
-    private void OnEnable()
-    {
-        pauseAction.action.Enable();
-        pauseAction.action.performed += TogglePause;
-    }
-
-    private void OnDisable()
-    {
-        pauseAction.action.performed -= TogglePause;
-        pauseAction.action.Disable();
-    }
-
-    private void TogglePause(InputAction.CallbackContext context)
-    {
-        isPaused = !isPaused;
-        if (isPaused)
-        {
-            PauseGame();
-        }
-        else
-        {
-            ResumeGame();
-        }
     }
 
     public void SetMusicVolume(float value)
@@ -72,31 +39,5 @@ public class AudioSettingsController : MonoBehaviour
     public void SetSFXVolume(float value)
     {
         audioMixer.SetFloat("SFXVolume", value);
-    }
-
-    void PauseGame()
-    {
-        Time.timeScale = 0f;
-        audioSettingsPanel.SetActive(true);
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
-
-        if (playerController != null)
-        {
-            playerController.SetInputEnabled(false);
-        }
-    }
-
-    void ResumeGame()
-    {
-        Time.timeScale = 1f;
-        audioSettingsPanel.SetActive(false);
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-
-        if (playerController != null)
-        {
-            playerController.SetInputEnabled(true);
-        }
     }
 }
